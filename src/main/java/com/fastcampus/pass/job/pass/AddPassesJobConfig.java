8db791eed62ee591e96f477961ac76050078d3c3
MintKim1990 <rlaalsxo1990@gmail.com>
2023-01-31 22:58:30 +0900

    feet: 대량 이용권 추가 Job 개발
     * Tasklet 기반에 대량이용권 추가 Job 개발
     * 벌크이용권 엔티티 추가
     * 사용자그룹 엔티티 추가
     * 테스트코드 추가

---
+++ src/main/java/com/fastcampus/pass/job/pass/AddPassesJobConfig.java
@@ -0,0 +1,50 @@
+package com.fastcampus.pass.job.pass;
+
+import com.fastcampus.pass.repository.pass.PassEntity;
+import com.fastcampus.pass.repository.pass.PassStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Configuration
+public class AddPassesJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final AddPassesTasklet addPassesTasklet;
+
+    public AddPassesJobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, AddPassesTasklet addPassesTasklet) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.addPassesTasklet = addPassesTasklet;
+    }
+
+    @Bean
+    public Job addPassesJob() {
+        return this.jobBuilderFactory.get("addPassesJob")
+                .start(addPassesStep())
+                .build();
+    }
+
+    @Bean
+    public Step addPassesStep() {
+        return this.stepBuilderFactory.get("addPassesStep")
+                .tasklet(addPassesTasklet)
+                .build();
+    }
+
+
+}
---
+++ src/test/java/com/fastcampus/pass/job/pass/AddPassesJobConfigTest.java
@@ -0,0 +1,96 @@
+package com.fastcampus.pass.job.pass;
+
+import com.fastcampus.pass.repository.pass.*;
+import com.fastcampus.pass.repository.user.UserGroupMappingEntity;
+import com.fastcampus.pass.repository.user.UserGroupMappingRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.repeat.RepeatStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@Slf4j
+@ExtendWith(MockitoExtension.class)
+class AddPassesJobConfigTest {
+
+    @Mock // AddPassesTasklet 클래스에 주입하기위해 Mock 선언
+    private StepContribution stepContribution;
+
+    @Mock // AddPassesTasklet 클래스에 주입하기위해 Mock 선언
+    private ChunkContext chunkContext;
+
+    @Mock // AddPassesTasklet 클래스에 주입하기위해 Mock 선언
+    private PassRepository passRepository;
+
+    @Mock // AddPassesTasklet 클래스에 주입하기위해 Mock 선언
+    private BulkPassRepository bulkPassRepository;
+
+    @Mock // AddPassesTasklet 클래스에 주입하기위해 Mock 선언
+    private UserGroupMappingRepository userGroupMappingRepository;
+
+    @InjectMocks
+    private AddPassesTasklet addPassesTasklet;
+
+    @Test
+    public void test_execute() {
+
+        // given
+        final String userGroupId = "GROUP";
+        final String userId = "A1000000";
+        final Integer packageSeq = 1;
+        final Integer count = 10;
+
+        final LocalDateTime now = LocalDateTime.now();
+
+        final BulkPassEntity bulkPassEntity = new BulkPassEntity();
+        bulkPassEntity.setPackageSeq(packageSeq);
+        bulkPassEntity.setUserGroupId(userGroupId);
+        bulkPassEntity.setStatus(BulkPassStatus.READY);
+        bulkPassEntity.setCount(count);
+        bulkPassEntity.setStartedAt(now);
+        bulkPassEntity.setEndedAt(now.plusDays(60));
+
+        final UserGroupMappingEntity userGroupMappingEntity = new UserGroupMappingEntity();
+        userGroupMappingEntity.setUserGroupId(userGroupId);
+        userGroupMappingEntity.setUserId(userId);
+
+        // when
+        // findByStatusAndStartedAtGreaterThan 메소드를 호출했을때 List.of(bulkPassEntity) 리턴되게 설정
+        when(bulkPassRepository.findByStatusAndStartedAtGreaterThan(eq(BulkPassStatus.READY), any())).thenReturn(List.of(bulkPassEntity));
+        // findByUserGroupId 메소드를 호출했을때 List.of(userGroupMappingEntity) 리턴되게 설정
+        when(userGroupMappingRepository.findByUserGroupId(eq("GROUP"))).thenReturn(List.of(userGroupMappingEntity));
+
+        RepeatStatus repeatStatus = addPassesTasklet.execute(stepContribution, chunkContext);
+
+        // then
+        assertEquals(RepeatStatus.FINISHED, repeatStatus);
+
+        // 추가된 PassEntity 값을 확인
+        ArgumentCaptor<List> passEntitiesCaptor = ArgumentCaptor.forClass(List.class);
+        verify(passRepository, times(1)).saveAll(passEntitiesCaptor.capture());
+        final List<PassEntity> passEntities = passEntitiesCaptor.getValue();
+
+        assertEquals(1, passEntities.size());
+        final PassEntity passEntity = passEntities.get(0);
+        assertEquals(packageSeq, passEntity.getPackageSeq());
+        assertEquals(userId, passEntity.getUserId());
+        assertEquals(PassStatus.READY, passEntity.getStatus());
+        assertEquals(count, passEntity.getRemainingCount());
+
+    }
+
+
+}
